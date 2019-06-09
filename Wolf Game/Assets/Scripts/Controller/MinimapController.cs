88f85e53182d3e2a6c8da7bafee1cccd5cbe5d2b
Oisín Headen <oisin.headen@gmail.com>
2019-06-10 09:08:26 +1000

    Changed to Minimapcontroller

---
+++ Wolf Game/Assets/Scripts/Controller/MinimapController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        // Find position in rect, send that to camera.
+        Debug.Log("Click");
+    }
+}
+
